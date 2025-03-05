1003a53beb4609a14614063877858aa2cee2a199
deltasampler <deltasampler@gmail.com>
2025-03-05 17:39:24 +0200

    update phys2 example

--- source/collision2.ts
+++ source/collision2.ts
@@ -1,8 +1,8 @@
 import {vec2_t, vec3_t} from "@cl/type.ts";
-import {cl_vec2, cl_vec2_dist, cl_vec2_set} from "@cl/vec2.ts";
+import {cl_vec2, cl_vec2_add, cl_vec2_copy, cl_vec2_dist, cl_vec2_set, cl_vec2_sub} from "@cl/vec2.ts";
 import {d2_aabb2, d2_center_transform, d2_circle2, d2_clear_color, d2_fill_vec, d2_init, d2_line2, d2_line_radius2, d2_obb2, d2_polygon_cent2, d2_reset_transform, d2_stroke_vec} from "@engine/d2.ts";
-import {io_init, io_m_move, m_event_t} from "@engine/io.ts";
-import {point_closest_aabb, point_closest_capsule, point_closest_circle, point_closest_convex_cent, point_closest_line, point_closest_obb, point_inside_aabb, point_inside_capsule, point_inside_circle, point_inside_convex_cent, point_inside_obb} from "@cl/phys2.ts";
+import {io_init, io_m_button_down, io_m_button_up, io_m_move, m_event_t} from "@engine/io.ts";
+import {line_intersect_aabb, line_intersect_capsule, line_intersect_circle, line_intersect_convex_cent, line_intersect_line, line_intersect_obb, point_closest_aabb, point_closest_capsule, point_closest_circle, point_closest_convex_cent, point_closest_line, point_closest_obb, point_inside_aabb, point_inside_capsule, point_inside_circle, point_inside_convex_cent, point_inside_obb} from "@cl/phys2.ts";
 import {cl_vec3} from "@cl/vec3.ts";
 
 document.body.style.height = "100vh";
@@ -23,14 +23,6 @@ const mouse = cl_vec2();
 
 io_init();
 
-io_m_move(function(event: m_event_t): void {
-    if (event.target !== canvas_el) {
-        return;
-    }
-
-    cl_vec2_set(mouse, event.x - canvas_el.width / 2.0, -event.y + canvas_el.height / 2.0);
-});
-
 d2_init(canvas_el);
 
 interface collider_t {
@@ -38,6 +30,9 @@ interface collider_t {
     point_inside(point: vec2_t): boolean;
     rotate(angle: number): void;
     closest_point(point: vec2_t): vec2_t;
+    translate(position: vec2_t): void;
+    get_position(): vec2_t;
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[];
 };
 
 class circle_t implements collider_t {
@@ -65,6 +60,18 @@ class circle_t implements collider_t {
     rotate(angle: number): void {
         return;
     }
+
+    translate(position: vec2_t): void {
+        cl_vec2_copy(this.position, position);
+    }
+
+    get_position(): vec2_t {
+        return this.position;
+    }
+
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[] {
+        return line_intersect_circle(this.position, this.radius, a, b);
+    }
 }
 
 class aabb_t implements collider_t {
@@ -91,6 +98,18 @@ class aabb_t implements collider_t {
 
     rotate(angle: number): void {
         return;
+    }
+
+    translate(position: vec2_t): void {
+        cl_vec2_copy(this.position, position);
+    }
+
+    get_position(): vec2_t {
+        return this.position;
+    }
+
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[] {
+        return line_intersect_aabb(this.position, this.size, a, b);
     }
 }
 
@@ -121,6 +140,18 @@ class obb_t implements collider_t {
     rotate(angle: number): void {
         this.angle += angle;
     }
+
+    translate(position: vec2_t): void {
+        cl_vec2_copy(this.position, position);
+    }
+
+    get_position(): vec2_t {
+        return this.position;
+    }
+
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[] {
+        return line_intersect_obb(this.position, this.size, this.angle, a, b);
+    }
 }
 
 class capsule_t implements collider_t {
@@ -150,6 +181,18 @@ class capsule_t implements collider_t {
     rotate(angle: number): void {
         return;
     }
+
+    translate(position: vec2_t): void {
+        return;
+    }
+
+    get_position(): vec2_t {
+        return this.start;
+    }
+
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[] {
+        return line_intersect_capsule(this.start, this.end, this.radius, a, b);
+    }
 }
 
 class line_t implements collider_t {
@@ -162,12 +205,12 @@ class line_t implements collider_t {
     }
 
     render(color: vec3_t): void {
-        d2_stroke_vec(color, 1.0);
+        d2_stroke_vec(color, 2.0);
         d2_line2(this.start, this.end);
     }
 
     point_inside(point: vec2_t): boolean {
-        return cl_vec2_dist(point_closest_line(this.start, this.end, point), point) <= 1.0;
+        return cl_vec2_dist(point_closest_line(this.start, this.end, point), point) <= 2.0;
     }
 
     closest_point(point: vec2_t): vec2_t {
@@ -176,6 +219,18 @@ class line_t implements collider_t {
 
     rotate(angle: number): void {
         return;
+    }
+
+    translate(position: vec2_t): void {
+        return;
+    }
+
+    get_position(): vec2_t {
+        return this.start;
+    }
+
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[] {
+        return line_intersect_line(this.start, this.end, a, b);
     }
 }
 
@@ -237,16 +292,112 @@ class polygon_t implements collider_t {
     rotate(angle: number): void {
         this.angle += angle;
     }
+
+    translate(position: vec2_t): void {
+        cl_vec2_copy(this.position, position);
+    }
+
+    get_position(): vec2_t {
+        return this.position;
+    }
+
+    intersect_line(a: vec2_t, b: vec2_t): vec2_t[] {
+        return line_intersect_convex_cent(this.points, this.position, this.angle, a, b);
+    }
 }
+
+const start = cl_vec2(-200.0, -200.0), end = cl_vec2(100.0, -400.0);
 
 const colliders: collider_t[] = [];
 colliders.push(new circle_t(cl_vec2(0.0), 50.0));
 colliders.push(new aabb_t(cl_vec2(200.0, 10.0), cl_vec2(120.0)));
 colliders.push(new obb_t(cl_vec2(-200.0, 10.0), cl_vec2(80.0, 160.0), 90.0));
 colliders.push(new capsule_t(cl_vec2(-200.0, 200.0), cl_vec2(200.0, 400.0), 30.0));
-colliders.push(new line_t(cl_vec2(-200.0, -200.0), cl_vec2(100.0, -400.0)));
 colliders.push(new polygon_t([cl_vec2(-100.0, -86.6), cl_vec2(100.0, -86.6), cl_vec2(0.0, 86.6)], cl_vec2(200.0, -200.0), 0.0));
 colliders.push(new polygon_t([cl_vec2(100.0, 0.0), cl_vec2(50.0, 86.6), cl_vec2(-50.0, 86.6), cl_vec2(-100.0, 0.0), cl_vec2(-50.0, -86.6), cl_vec2(50.0, -86.6)], cl_vec2(-500.0, -200.0), 0.0));
+colliders.push(new line_t(cl_vec2(-400.0, 100.0), cl_vec2(-200.0, 300.0)));
+
+const line = new line_t(start, end);
+colliders.push(line);
+
+let selected: collider_t|null = null;
+let start_position = cl_vec2();
+let selected_pos = cl_vec2();
+let capsule_part: number = 0;
+
+io_m_move(function(event: m_event_t): void {
+    if (event.target !== canvas_el) {
+        return;
+    }
+
+    cl_vec2_set(mouse, event.x - canvas_el.width / 2.0, -event.y + canvas_el.height / 2.0);
+
+    if (selected) {
+        if (selected instanceof capsule_t || selected instanceof line_t) {
+            if (capsule_part === 1) {
+                cl_vec2_copy(selected.start, mouse);
+            } else if (capsule_part === 2) {
+                cl_vec2_copy(selected.end, mouse);
+            }
+        } else {
+            const offset = cl_vec2_sub(mouse, start_position);
+            selected.translate(cl_vec2_add(selected_pos, offset));
+        }
+    }
+});
+
+io_m_button_down(function(event: m_event_t): void {
+    if (event.target !== canvas_el) {
+        return;
+    }
+
+    for (const collider of colliders) {
+        if (collider instanceof capsule_t) {
+            if (point_inside_circle(collider.start, collider.radius, mouse)) {
+                capsule_part = 1;
+                selected = collider;
+                return;
+            } else if (point_inside_circle(collider.end, collider.radius, mouse)) {
+                capsule_part = 2;
+                selected = collider;
+                return;
+            } else {
+                capsule_part = 0;
+            }
+        }
+
+        if (collider instanceof line_t) {
+            if (point_inside_circle(collider.start, 20.0, mouse)) {
+                capsule_part = 1;
+                selected = collider;
+
+                return;
+            } else if (point_inside_circle(collider.end, 20.0, mouse)) {
+                selected = collider;
+                capsule_part = 2;
+                return;
+            } else {
+                capsule_part = 0;
+            }
+        }
+
+        if (collider.point_inside(mouse)) {
+            selected = collider;
+            cl_vec2_copy(start_position, mouse);
+            cl_vec2_copy(selected_pos, collider.get_position());
+
+            return;
+        }
+    }
+});
+
+io_m_button_up(function(event: m_event_t): void {
+    if (event.target !== canvas_el) {
+        return;
+    }
+
+    selected = null;
+});
 
 function update(): void {
     for (const collider of colliders) {
@@ -260,6 +411,11 @@ function render(): void {
     d2_center_transform();
 
     for (const collider of colliders) {
+        if (collider === line) {
+            collider.render(cl_vec3(89, 111, 255));
+            continue;
+        }
+
         if (collider.point_inside(mouse)) {
             collider.render(cl_vec3(227, 227, 227));
         } else {
@@ -276,6 +432,15 @@ function render(): void {
 
             d2_fill_vec(cl_vec3(255, 120, 120));
             d2_circle2(cp, 4.0);
+        }
+
+        if (collider !== line) {
+            const points = collider.intersect_line(line.start, line.end);
+
+            for (const point of points) {
+                d2_fill_vec(cl_vec3(255, 120, 120));
+                d2_circle2(point, 4.0);
+            }
         }
     }
 }
